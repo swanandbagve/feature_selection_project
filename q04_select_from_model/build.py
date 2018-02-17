6fe9d1d3d9658a60273ab7ab4908fde150489750
swanandbagve <swanandbagve@gmail.com>
2018-02-17 11:36:13 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -8,3 +8,15 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(df):
+    X,y=data.iloc[:,:-1],data.iloc[:,-1]
+
+    clf=RandomForestClassifier()
+    clf.fit(X, y)
+    model=SelectFromModel(clf, prefit=True)
+    df1= pd.DataFrame(np.vstack((X.columns.T,model.get_support())))
+    #return type(list(df1[df1.Significant<=1].Features))
+    df1.columns=df1.iloc[0]
+    df1= df1.T
+    df1.columns=[['Features','Significance']]
+    return list(df1[df1.Significance==True]['Features'])
